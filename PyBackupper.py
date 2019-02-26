1bd464651097389ccdc639ed4133ad73ad00e646
Chip <chipz68q@gmail.com>
2019-02-26 10:42:08 +0500

    Add zipbackup constructor & destructor

--- PyBackupper.py
+++ PyBackupper.py
@@ -1,8 +1,7 @@
 from zipbackup import *
 
 print("PyBackupper: 1.0.0.0b")
-zip = zipbackup()
-zip.filename = "backup1.zip"
-zip.path = "/home/chip/python/"
+zip = zipbackup("backup2.zip", "/home/chip/python")
 print(zip.Bzipfile(zip.filename, zip.path))
 print(zip.Bextract(zip.filename, zip.path))
+del zip
--- zipbackup.py
+++ zipbackup.py
@@ -1,6 +1,11 @@
 class zipbackup(object):
-    filename = ""
-    path = ""
+
+    def __init__(self, filename_, path_):
+        self.filename = filename_
+        self.path = path_
+
+    def __del__(self):
+        print("Экземпляр класса уничтожен")
 
     def Bzipfile(self, filename, path):
         return "Файлы по пути " + path + " архивированы в архив " + filename
