92e8820f5cf522d9688021c2eb12167cdf4089db
Chip <chipz68q@gmail.com>
2019-03-07 23:52:20 +0500

    Add try-except in main

--- PyBackupper.py
+++ PyBackupper.py
@@ -1,13 +1,18 @@
 from zipbackup import *
 import sys
 
-print("PyBackupper: 1.0.0.0e")
-if len(sys.argv) > 3:
-    if sys.argv[1] == "-a":
-        zip = zipbackup(sys.argv[2], sys.argv[3], 'w')
-        print(zip.Bzipfile())
-        del zip
-    elif sys.argv[1] == "-e":
-        zip = zipbackup(sys.argv[2], sys.argv[3], 'r')
-        print(zip.Bextract())
-        del zip
+print("PyBackupper: 1.0.0.0h")
+try:
+    if len(sys.argv) > 3:
+        if sys.argv[1] == "-a":
+            zip = zipbackup(sys.argv[2], sys.argv[3], 'w')
+            print(zip.Bzipfile())
+            del zip
+        elif sys.argv[1] == "-e":
+            zip = zipbackup(sys.argv[2], sys.argv[3], 'r')
+            print(zip.Bextract())
+            del zip
+except FileNotFoundError:
+    print("Файла " + sys.argv[2] + " не существует")
+except Exception as ex:
+    print('Error:', str(ex))
