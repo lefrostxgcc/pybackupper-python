6aa1b7e0f748a48240ee600dfbce7a045b7bbd3f
Chip <chipz68q@gmail.com>
2019-03-07 18:55:40 +0500

    Add Bzipfile, use argv

--- PyBackupper.py
+++ PyBackupper.py
@@ -1,7 +1,13 @@
 from zipbackup import *
+import sys
 
-print("PyBackupper: 1.0.0.0b")
-zip = zipbackup("/home/chip/python/test/zip/backup_2.zip", "/home/chip/python", "r")
-#print(zip.Bzipfile(zip.filename, zip.path))
-print(zip.Bextract())
-del zip
+print("PyBackupper: 1.0.0.0e")
+if len(sys.argv) > 3:
+    if sys.argv[1] == "-a":
+        zip = zipbackup(sys.argv[2], sys.argv[3], 'w')
+        print(zip.Bzipfile())
+        del zip
+    elif sys.argv[1] == "-e":
+        zip = zipbackup(sys.argv[2], sys.argv[3], 'r')
+        print(zip.Bextract())
+        del zip
--- zipbackup.py
+++ zipbackup.py
@@ -11,8 +11,18 @@ class zipbackup(object):
     def __del__(self):
         print("Экземпляр класса уничтожен")
 
-    def Bzipfile(self, filename, path):
-        return "Файлы по пути " + path + " архивированы в архив " + filename
+    def Bzipfile(self):
+        result = ""
+        try:
+            wr = zipfile.ZipFile(self.filename, self.archtype)
+            for root, dirs, files in os.walk(self.path):
+                for file in files:
+                    wr.write(os.path.join(root, file))
+                    result = "Файлы по пути " + self.path + " архивированы в архив " + self.filename
+            wr.close()
+        except Exception as Ex:
+            result = "Error: " + str(Ex)
+        return result
 
     def Bextract(self):
         result = ""
@@ -20,6 +30,7 @@ class zipbackup(object):
             wr = zipfile.ZipFile(self.filename, self.archtype)
             wr.extractall(self.path)
             result = "Архив " + self.filename + " распакован в каталог " + self.path
+            wr.close()
         except Exception as Ex:
             result = "Error: " + str(Ex)
         return result
