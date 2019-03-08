f81eab8095c5daa8c55415f1976423a4db68e283
Chip <chipz68q@gmail.com>
2019-03-08 15:25:05 +0500

    Add Bbackup

--- PyBackupper.py
+++ PyBackupper.py
@@ -4,14 +4,9 @@ import sys
 print("PyBackupper: 1.0.0.0h")
 try:
     if len(sys.argv) > 3:
-        if sys.argv[1] == "-a":
-            zip = zipbackup(sys.argv[2], sys.argv[3], 'w')
-            print(zip.Bzipfile())
-            del zip
-        elif sys.argv[1] == "-e":
-            zip = zipbackup(sys.argv[2], sys.argv[3], 'r')
-            print(zip.Bextract())
-            del zip
+        zip = zipbackup(sys.argv[2], sys.argv[3], sys.argv[1])
+        print(zip.Bbackup())
+        del zip
 except FileNotFoundError:
     print("Файла " + sys.argv[2] + " не существует")
 except Exception as ex:
--- zipbackup.py
+++ zipbackup.py
@@ -3,34 +3,49 @@ import os
 
 class zipbackup(object):
 
-    def __init__(self, filename_, path_, archtype_):
+    def __init__(self, filename_, path_, command_):
         self.filename = filename_
         self.path = path_
-        self.archtype = archtype_
+        self.archtype = 'r'
+        if command_ == '-a':
+            self.archtype = 'w'
+        elif command_ == '-e':
+            self.archtype = 'r'
+        elif command_ == '-v':
+            self.archtype = 'r'
+        self.command = command_
 
     def __del__(self):
         print("Экземпляр класса уничтожен")
 
-    def Bzipfile(self):
+    def Bbackup(self):
         result = ""
         try:
             wr = zipfile.ZipFile(self.filename, self.archtype)
-            for root, dirs, files in os.walk(self.path):
-                for file in files:
-                    wr.write(os.path.join(root, file))
-                    result = "Файлы по пути " + self.path + " архивированы в архив " + self.filename
+            if self.command == '-a':
+                result = self.addarchive(wr)
+            elif self.command == '-e':
+                result = self.extract(wr)
+            elif self.command == '-v':
+                result = self.view(wr)
             wr.close()
         except Exception as Ex:
             result = "Error: " + str(Ex)
         return result
 
-    def Bextract(self):
+    def addarchive(self, wr):
+        for root, dirs, files in os.walk(self.path):
+            for file in files:
+                wr.write(os.path.join(root, file))
+        result = "Файлы по пути " + self.path + " архивированы в архив " + self.filename
+        return result
+
+    def extract(self, wr):
+        wr.extractall(self.path)
+        result = "Архив " + self.filename + " распакован в каталог " + self.path
+        return result
+
+    def view(self, wr):
+        wr.printdir()
         result = ""
-        try:
-            wr = zipfile.ZipFile(self.filename, self.archtype)
-            wr.extractall(self.path)
-            result = "Архив " + self.filename + " распакован в каталог " + self.path
-            wr.close()
-        except Exception as Ex:
-            result = "Error: " + str(Ex)
         return result
